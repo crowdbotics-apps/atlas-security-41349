0001d350bbf92bb12798bc517be5d13457ebfcdd
Crowdbotics <team@crowdbotics.com>
2023-06-07 20:32:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/atlas_security_41349/MainActivity.java
@@ -0,0 +1,15 @@
+package com.atlas_security_41349;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "atlas_security_41349";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "atlas-security-41349",
+  url: "https://atlas-security-41349.botics.co",
+  api: "https://atlas-security-41349.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
